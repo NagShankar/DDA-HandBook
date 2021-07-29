6d3b1929fd489b0ae1bd34f62f7aa6a039e13f3a
NagShankar <nag.shankar6@gmail.com>
2021-07-29 12:48:29 +0530

    initial

---
+++ src/pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="main-header">
+   <div class="logo">
+        <img class="main-logo" src="../assets/logo.svg" alt="DDA Handbook" />
+   </div> 
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.main-header{
+    height: 100vh;
+    background-color: #b9e3ea;
+    display: flex;
+    justify-content: center;
+    align-items: center; 
+}
+.logo {
+   width: 30vw;
+   height: 30vh;
+}
+.main-logo{width: 100%;height: 100%; display: block;}
+</style>
